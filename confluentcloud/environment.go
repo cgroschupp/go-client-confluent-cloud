d9bc153ee7b772766011a1eebec0ae0d6b61dcad
Christian Groschupp <christian@groschupp.org>
2020-01-22 09:53:48 +0100

    add environment

---
+++ confluentcloud/environment.go
@@ -0,0 +1,133 @@
+package confluentcloud
+
+import (
+	"fmt"
+	"net/url"
+)
+
+type Environment struct {
+	ID             string `json:"id"`
+	Name           string `json:"name"`
+	OrganizationID int    `json:"organization_id"`
+	Deactivated    bool   `json:"deactivated"`
+}
+
+type EnvironmentResponse struct {
+	Account Environment `json:"account"`
+}
+
+type EnvironmentCreateRequest struct {
+	Account EnvironmentRequest `json:"account"`
+}
+
+type EnvironmentRequest struct {
+	Name           string `json:"name"`
+	OrganizationID int    `json:"organization_id"`
+}
+
+func (c *Client) GetEnvironment(id string) (*Environment, error) {
+	rel, err := url.Parse(fmt.Sprintf("accounts/%s", id))
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	response, err := c.NewRequest().
+		SetResult(&EnvironmentResponse{}).
+		SetError(&ErrorResponse{}).
+		Get(u.String())
+
+	if err != nil {
+		return nil, err
+	}
+
+	if response.IsError() {
+		return nil, fmt.Errorf("get environment: %s", response.Error().(*ErrorResponse).Error.Message)
+	}
+
+	return &response.Result().(*EnvironmentResponse).Account, nil
+}
+
+func (c *Client) CreateEnvironment(name string, organizationID int) (*Environment, error) {
+	rel, err := url.Parse("accounts")
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	request := EnvironmentRequest{
+		Name:           name,
+		OrganizationID: organizationID,
+	}
+
+	response, err := c.NewRequest().
+		SetBody(&EnvironmentCreateRequest{Account: request}).
+		SetResult(&EnvironmentResponse{}).
+		SetError(&ErrorResponse{}).
+		Post(u.String())
+
+	if err != nil {
+		return nil, err
+	}
+
+	if response.IsError() {
+		return nil, fmt.Errorf("environment: %s", response.Error().(*ErrorResponse).Error.Message)
+	}
+
+	return &response.Result().(*EnvironmentResponse).Account, nil
+}
+
+func (c *Client) DeleteEnvironment(id string) error {
+	rel, err := url.Parse(fmt.Sprintf("accounts/%s", id))
+	if err != nil {
+		return err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	response, err := c.NewRequest().
+		SetError(&ErrorResponse{}).
+		Delete(u.String())
+
+	if err != nil {
+		return err
+	}
+
+	if response.IsError() {
+		return fmt.Errorf("delete environment: %s", response.Error().(*ErrorResponse).Error.Message)
+	}
+
+	return nil
+}
+
+func (c *Client) UpdateEnvironment(id, newName string, organizationID int) (*Environment, error) {
+	rel, err := url.Parse(fmt.Sprintf("accounts/%s", id))
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	request := EnvironmentRequest{
+		Name:           newName,
+		OrganizationID: organizationID,
+	}
+
+	response, err := c.NewRequest().
+		SetBody(&EnvironmentCreateRequest{Account: request}).
+		SetResult(&EnvironmentResponse{}).
+		SetError(&ErrorResponse{}).
+		Put(u.String())
+
+	if err != nil {
+		return nil, err
+	}
+
+	if response.IsError() {
+		return nil, fmt.Errorf("environment: %s", response.Error().(*ErrorResponse).Error.Message)
+	}
+
+	return &response.Result().(*EnvironmentResponse).Account, nil
+}
